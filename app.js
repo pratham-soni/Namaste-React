b69fccee5a6f6ce2a1e1a201a44ee759bced1105
pratham-soni <spratham304@gmail.com>
2025-03-16 10:58:27 +0530

    intorducing jsx and components

--- app.js
+++ app.js
@@ -1,39 +1,36 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
-// for creating an element h1 using react
-const heading = React.createElement("h1", {id: "heading", xyz: "abc"}, "Hello world using react from external file!");
+/*
+React Elements
+const heading = React.createElement("h1", {id:"root"}, "I am created using React.creareElement")
 
+JSX => javascript syntax, which can create the react element eaisier that React.createElement()
+JSX is not HTML inside javascript. It is HTML Like syntax
+console.log(heading)
+*/
 
-// adding multiple childs
-const parent = React.createElement(
-    "div",
-    {id: "parent"},
-    [
-        React.createElement(
-            "div",
-            {id: "clild1"},
-            [
-                React.createElement("h1",{},"I am a h1 tag"),
-                React.createElement("h2",{},"I am a h2 tag for nested")
-            ]
-        ),
-        React.createElement(
-            "div",
-            {id: "clild2"},
-            [
-                React.createElement("h1",{},"I am a h1 tag"),
-                React.createElement("h2",{},"I am a h2 tags")
-            ]
-        )
-    ]
-)
+// React Element
+const jsxHeading = (
+  <h1 id="root" className="head">
+    I am created using JSX
+  </h1>
+);
 
-// for creating a root for react 
+// above line is not pure JS, JS Engine will not understand html code directly.
+/*
+* JSX will be transpiled before it reaches to browser ( ie, root.render()) - Parcel gives this job to Babel
+Babel - Trasnpiler
+JSX => React.createElement() => object => HTML 
+* Bable - is a node library - can transpile the ES6 code to older browser's compatible code
+we use camelCasing for giving attribute into the JSX
+H.W. -> how to write different tags (a, img, src etc) inside jsx
+*/
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// randering root and injecting the element we created "heading".
-// root.render(heading)
+root.render(jsxHeading);
 
-console.log(parent)
-root.render(parent)
+// React Component
