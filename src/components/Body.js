deec4e9edbb72750ed809f7969e613dc8eee76b0
pratham-soni <spratham304@gmail.com>
2025-03-25 18:27:14 +0530

    added cros proxy in url

--- src/components/Body.js
+++ src/components/Body.js
@@ -24,7 +24,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      "https://proxy.corsfix.com/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(
