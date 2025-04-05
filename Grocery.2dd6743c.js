ea593e87191d17c053da206373269d95ca39eccd
Raj Vashisht <raj.vashisht@oracle.com>
2025-04-05 21:40:01 +0530

    Updates

---
+++ Grocery.2dd6743c.js
@@ -0,0 +1,2 @@
+var e=globalThis.parcelRequire94c2;(0,e.register)("loR3J",function(r,o){Object.defineProperty(r.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(r.exports,"default",{get:()=>l,set:void 0,enumerable:!0,configurable:!0});var i=e("ayMG0"),l=()=>(0,i.jsxs)("div",{children:[(0,i.jsx)("h1",{children:"Our Grocery Store Inside food ordering app"}),(0,i.jsx)("h2",{children:"more complex components"})]})});
+//# sourceMappingURL=Grocery.2dd6743c.js.map
